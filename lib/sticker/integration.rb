84fdab09b4ded57495b5009f3fa10b5b34a7ce3c
Giau Nguyen <giau.nguyen@mingle.com>
2018-06-19 17:19:53 +0700

    Initial commit

---
+++ lib/sticker/integration.rb
@@ -0,0 +1,7 @@
+require "sticker/integration/version"
+
+module Sticker
+  module Integration
+    # Your code goes here...
+  end
+end
---
+++ lib/sticker/integration/version.rb
@@ -0,0 +1,5 @@
+module Sticker
+  module Integration
+    VERSION = "0.1.0"
+  end
+end
